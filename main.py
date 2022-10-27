4c8c6e3ef6dc0abbde394a3a7d857ff748c2aec9
Kevin Santiago <113930456+ksantiago772@users.noreply.github.com>
2022-10-27 17:43:40 +0000

    .

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
 # from methods_help import method_help
-from functions import  welcome, add_numbers,num_dividedFrom_add_numbers, greet 
+from functions import  welcome, add_numbers,num_dividedFrom_add_numbers, greet , square
 # from returnStuff import returnS
 
 # method_help()
 welcome ("yahaira")
 added = add_numbers(45,5)
 print(added)
-
 final = num_dividedFrom_add_numbers(45,added)
 print(final)
 greet()
-
+square(7)
 
 # returnS()
